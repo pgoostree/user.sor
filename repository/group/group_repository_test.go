56c6ccc9916ae4095d94c1beda3a9e63180ff47d
Peter Goostree <pgoostree@cylance.com>
2020-06-08 17:20:37 -0700

    add unit test for delete group

--- repository/group/group_repository_test.go
+++ repository/group/group_repository_test.go
@@ -60,3 +60,30 @@ func TestShouldGetGroup(t *testing.T) {
 		t.Errorf("there were unfulfilled expectations: %s", err)
 	}
 }
+
+func TestShouldDeleteGroup(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error %s was no expected when opening a stub db connection", err)
+	}
+	defer db.Close()
+
+	rows := sqlmock.NewRows([]string{"out_rowsaffected"}).
+		AddRow(1)
+
+	groupName := "testGroupName"
+	grouprepo := GroupRepository{}
+
+	mock.ExpectQuery("users.delete_group").
+		WithArgs(groupName).
+		WillReturnRows(rows)
+
+	if _, err = grouprepo.DeleteGroup(db, groupName); err != nil {
+		t.Errorf("error was not expected while deleting group: %s", err)
+	}
+
+	// we make sure that all expectations were met
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
