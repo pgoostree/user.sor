18d4a7ed0facd1e9a5c6220d165902668f9d109c
Peter Goostree <pgoostree@cylance.com>
2020-06-08 17:17:33 -0700

    add unit test for get user group

--- repository/group/group_repository_test.go
+++ repository/group/group_repository_test.go
@@ -33,3 +33,30 @@ func TestShouldInsertGroup(t *testing.T) {
 		t.Errorf("there were unfulfilled expectations: %s", err)
 	}
 }
+
+func TestShouldGetGroup(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error %s was no expected when opening a stub db connection", err)
+	}
+	defer db.Close()
+
+	rows := sqlmock.NewRows([]string{"user_id", "group_name"}).
+		AddRow("testGroupId", "testGroupName")
+
+	groupName := "testGroupName"
+	grouprepo := GroupRepository{}
+
+	mock.ExpectQuery("users.get_user_group").
+		WithArgs(groupName).
+		WillReturnRows(rows)
+
+	if _, err = grouprepo.GetGroup(db, groupName); err != nil {
+		t.Errorf("error was not expected while getting user: %s", err)
+	}
+
+	// we make sure that all expectations were met
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
